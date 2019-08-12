c24ce79c7c5826c5a3a5e8f39f498c7ba822b9fc
Tina B <Tina.Brownfield@cdk.com>
2019-08-11 19:02:00 -0700

    add lesson8 assignment 1 xss update

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import base64
+import html
 
 from flask import Flask, request
 from model import Message 
@@ -25,13 +26,14 @@ def home():
 
 <h2>Wisdom From Your Fellow Classmates</h2>
 """
-    
+    s = html.escape( """& < " ' >""" )
     for m in Message.select():
         body += """
 <div class="message">
 {}
 </div>
-""".format(m.content)
+""".format(m.content.replace('<', '&lt;').replace('>', '&gt;').replace('&', '&amp;').replace('"','&quot;'))
+
 
     return body 
 
